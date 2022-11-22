20127b5653dddb0c9983df08d0704d0485ce22a8
iryna-zatynina <ira.zat1997@gmail.com>
2022-11-22 15:26:42 +0100

    add components

---
+++ src/pages/Page404/Page404.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Error404 from "../../сomponents/Error404/Error404";
+
+const Page404 = () => {
+    return (
+        <>
+            <Error404 />
+        </>
+    );
+};
+
+export default Page404;
